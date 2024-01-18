2d10069baee1a79d5cb3e3221406318488e0c659
MinaEhab77 <fouadattia09@gmail.com>
2024-01-18 13:52:51 -0800

    updated

---
+++ executee.c
@@ -0,0 +1,46 @@
+#include "monty.h"
+
+/**
+* execute - execute the code
+* @stack: head stack
+* @counter: counter
+* @file: poiner monty
+* @content: line_content
+*/
+
+int executee(char *content, stack_t **stack, unsigned int counter, FILE *file)
+{
+	instruction_t opst[] = {
+				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
+				{"pop", f_pop},
+				{"swap", f_swap},
+				{"add", f_add},
+				{"nop", f_nop},
+				{"sub", f_sub},
+				{"queue", f_queue},
+				{"stack", f_stack},
+				{NULL, NULL}
+				};
+	unsigned int k = 0;
+	char *op;
+
+	op = strtok(content, " \n\t");
+	if (op && op[0] == '#')
+		return (0);
+	bus.arg = strtok(NULL, " \n\t");
+	while (opst[k].opcode && op)
+	{
+		if (strcmp(op, opst[i].opcode) == 0)
+		{	opst[k].f(stack, counter);
+			return (0);
+		}
+		i++;
+	}
+	if (op && opst[k].opcode == NULL)
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+		fclose(file);
+		free(content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE); }
+	return (1);
+}
